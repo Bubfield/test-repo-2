9e5b819aa40d3a45120b6aa1508fca3fc83401d8
Bubfield <69983820+Bubfield@users.noreply.github.com>
2022-11-21 13:42:03 -0600

    Delete index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-//import { render } from '@testing-library/react';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import  App from './App';
-import './index.css';
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
